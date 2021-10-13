2d042ae3427f1d3846435a7d364a61bf7f755c27
NMBR78 <newsofthelastwave@gmail.com>
2021-10-14 01:41:06 +0300

    update_pages
    add page feedback (form email and form textarea)
    
    update pages about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,59 @@
 import React, { Component } from "react";
+import { Container, Tab, Nav, Row, Col } from "react-bootstrap";
 
 export default class About extends Component {
     render() {
         return (
-            <div>
-                About us
-            </div>
+            <Container>
+                <Tab.Container id="ledt-tabs-examle" defaultActiveKey="first">
+                    <Row>
+                        <Col sm={3}>
+                            <Nav variant="pills" className="flex-column mt-2">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Design</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Team</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">Programming</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="fourth">Fraemworks</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="fifth">Libraries</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        <Col sm={9}>
+                            <Tab.Content className="mt-3">
+                                <Tab.Pane eventKey="first">
+                                    <img src="https://img5.goodfon.com/original/800x480/0/85/anna-shuvalova-devushka-ryzhaia-plate-perednik-derevia-manda.jpg"></img>
+                                    <p>Text</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    <img src="https://img5.goodfon.com/original/800x480/0/85/anna-shuvalova-devushka-ryzhaia-plate-perednik-derevia-manda.jpg"></img>
+                                    <p>Text</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    <img src="https://img5.goodfon.com/original/800x480/0/85/anna-shuvalova-devushka-ryzhaia-plate-perednik-derevia-manda.jpg"></img>
+                                    <p>Text</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="fourth">
+                                    <img src="https://img5.goodfon.com/original/800x480/0/85/anna-shuvalova-devushka-ryzhaia-plate-perednik-derevia-manda.jpg"></img>
+                                    <p>Text</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="fifth">
+                                    <img src="https://img5.goodfon.com/original/800x480/0/85/anna-shuvalova-devushka-ryzhaia-plate-perednik-derevia-manda.jpg"></img>
+                                    <p>Text</p>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+                </Tab.Container>
+            </Container>
+
         )
     }
 }
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import { Container, Form, Button } from "react-bootstrap";
 
 export default class Contacts extends Component {
     render() {
         return (
-            <div>
-                Contacts
-            </div>
+            <Container style={{ width: '500px'}}>
+                <h1 className="text-center"> Contact us</h1>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter mail"/>
+                        <Form.Text>
+                         he-he, I know you email!
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Example textarea</Form.Label>
+                        <Form.Control as="textarea" rows="10" cols="30"/>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">Sumbit</Button>
+
+                </Form>
+            </Container>
         )
     }
 }
