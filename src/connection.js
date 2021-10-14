8d15f778d392f7bd2b8e3d978fe8203c1b54ee92
NMBR78 <newsofthelastwave@gmail.com>
2021-10-14 20:12:46 +0300

    create mysql2

--- src/connection.js
+++ src/connection.js
@@ -1,15 +1,28 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : '92.205.7.58',
-  user     : 'alexeyuser001',
-  password : '48hfuhrf7374hf0hhf'
+// get the client
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: '92.205.7.58',
+  user: 'alexeyuser001',
+  password: '48hfuhrf7374hf0hhf',
+  database: 'dbfeedback'
 });
 
- connection.connect(function(err){
-        if (err) {
-          return console.error("Ошибка: " + err.message);
-        }
-        else{
-          console.log("Подключение к серверу MySQL успешно установлено");
-        }
-     });
+// // simple query
+// connection.query(
+//   'SELECT * FROM `person` WHERE `name` = "Page" AND `age` > 45',
+//   function(err, results, fields) {
+//     console.log(results); // results contains rows returned by server
+//     console.log(fields); // fields contains extra meta data about results, if available
+//   }
+// );
+
+// // with placeholder
+// connection.query(
+//   'SELECT * FROM `person` WHERE `name` = ? AND `age` > ?',
+//   ['Page', 45],
+//   function(err, results) {
+//     console.log(results);
+//   }
+// );
